15b3d4401f6811e45611e48763a7783acc3f3d6e
Sonu Kumar Keshri <75078531+Sonukumarkeshri4@users.noreply.github.com>
2021-01-18 00:55:31 +0530

    Add files via upload

---
+++ js/custom/faq.js
@@ -0,0 +1,15 @@
+const items = document.querySelectorAll(".accordion a");
+
+ 
+
+function toggleAccordion(){
+
+  this.classList.toggle('active');
+
+  this.nextElementSibling.classList.toggle('active');
+
+}
+
+ 
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
